c9b0787c628c1165b7684c85886803bb0d2626fa
Juvi Panaguiton <juviscript@gmail.com>
2026-06-10 21:32:12 -0400

    chore: initial Spring Boot project scaffold

---
+++ api/src/main/java/dev/juviscript/klutchgaming/KlutchGamingPlatformApplication.java
@@ -0,0 +1,13 @@
+package dev.juviscript.klutchgaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlutchGamingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlutchGamingPlatformApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/juviscript/klutchgaming/KlutchGamingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juviscript.klutchgaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlutchGamingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
